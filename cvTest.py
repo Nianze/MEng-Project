2848bf6951bfbdb7eb5937f82402a46987cf8ae2
Joseph <nl443@cornell.edu>
2015-10-02 09:19:10 -0400

    face detected function added

--- cvTest.py
+++ cvTest.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np 
 
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 cap = cv2.VideoCapture('test_k5.AVI')
 font = cv2.FONT_HERSHEY_SIMPLEX
 drawing = False # true if mouse is pressed
@@ -22,6 +23,18 @@ cv2.setMouseCallback('image', draw)
 
 while (cap.isOpened()):
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Draw a rectangle around the target area selected by mouse   
     if drawing == True:
     	cv2.rectangle(frame, (ix,iy),(ex,ey),(0,255,0),2)
         cv2.putText(frame, '  Target confirmed!',(ix,(ey+iy)//2),font,0.5,(0,0,255),1,cv2.CV_AA)
@@ -32,6 +45,6 @@ while (cap.isOpened()):
       break
     if key == ord(' '):
       cv2.imwrite('croped image.jpg', crop_im)
-# when finished
+# when finished, release the capture
 cap.release()
 cv2.destroyAllWindows()
